4d4d2ae3c1f232e5d69defdc56f31d83cc036a97
Anish Tondwalkar <2012atondwal@tjhsst.edu>
2011-08-11 09:46:54 -0400

    Cleaned up multistation plotting code

--- multistation.m
+++ multistation.m
@@ -1,10 +1,6 @@
 (* ::Package:: *)
 
 (* Anish Tondwalkar, NRL *)
-(*
-Initial Release on:Tue Aug  2 15:45:31 EDT 2011
-*)
-
 dir= "/home/atondwal/Ionosonde data 2008/bins/";
 TIMEDIR="/home/atondwal/Sami Data from Sarah/time/";
 Dir="/home/atondwal/Sami Data from Sarah/nmf2hmf2-1d/";
@@ -25,42 +21,46 @@ SetDirectory@dir;
 <<("stations"<>ToString@year<>".bin")
 *)
 
+
 (*The data has a lot entries like:*)
 (*	2008-03-03 15:00 5.15 "/" "/"*)
 (*	2008-03-03 15:00 3.85 "/" "/"*)
 (*so we'll just throw away the stations with duplicates*)
+(*midlat*)
 dropbads[list_]:=Fold[Drop,list,Reverse@{{11},{13},{15},{17}}]
+(*no bads*)
 (*dropbads:=#&*)
+(*equatorial*)
 (*dropbads[list_]:=Drop[list,{2}]*)
 
 
-(*Throw away the ones not in the interested interval*)
+(*Throw away the ones that aren't WHI *)
 hmF2C=Cases[#,{t_,h_}/;62<t<107]&/@hmF2;
 foF2C=Cases[#,{t_,h_}/;62<t<107]&/@foF2;
+
 
 (*Takes a function and a (time,val) pair, and finds the (time, difference at time) *)
 diffs[ifunc_,list_]:={#1,ifunc[#1]-#2}&@@@list
 (*Maps (time, diff) to (time,% diff) *)
-diffper[diffdata_]:=Transpose/@Transpose@{Transpose[#][[1]]&/@diffdata ,Transpose[#][[2]]&/@(100diffdata/hmF2)};
+diffper[diffdata_]:=Transpose/@Transpose@{Transpose[#][[1]]&/@diffdata ,Transpose[#][[2]]&/@(100 diffdata/hmF2)};
 perplots[diffper_]=ListPlot[#,PlotRange->{{62,68},{-100,100}}]&/@diffper;
 (*UTC to localtime*)
 local[list_,lon_]:={#1-lon/(15*24),#2}&@@@list;
 tolocal[diffdatac_]:=local@@@Transpose[dropbads/@{diffdatac,Transpose[stations][[3]]}];
 
 
-(* ::Input:: *)
-(*diffhmF2=diffs@@@Transpose@{dropbads[IhmF2],dropbads[hmF2]};*)
-(*difffoF2=diffs@@@Transpose@{dropbads[IfoF2],dropbads[foF2]};*)
-(*diffplots=ListPlot[#1,Frame -> True, Axes -> False, FrameLabel->{"Day of Year","hmF2 (km)",#2[[1]],None},PlotRange->{{62,68},{-300,300}},ImageSize->600] & @@@ Transpose@{diffhmF2~Join~difffoF2,*)
-(*stations};*)
-(*diffhmF2c=diffs@@@Transpose@{IhmF2,hmF2C};*)
-(*difffoF2c=diffs@@@Transpose@{IfoF2,foF2C};*)
-(*Dimensions[diffhmF2hr=GatherBy[#,IntegerPart[24FractionalPart[#[[1]]]]&]&/@tolocal[diffhmF2c]]*)
-(*Dimensions[difffoF2hr=GatherBy[#,IntegerPart[24FractionalPart[#[[1]]]]&]&/@tolocal[difffoF2c]]*)
-(*Dimensions[diffhmF2day=GatherBy[Join@@tolocal[diffhmF2c],IntegerPart[#[[1]]]&]]*)
-(*Dimensions[difffoF2day=GatherBy[Join@@tolocal[difffoF2c],IntegerPart[#[[1]]]&]]*)
-(*Dimensions[diffhmF2dayhr=GatherBy[#,8<IntegerPart[24FractionalPart[#[[1]]]]<17&]&/@diffhmF2day]*)
-(*Dimensions[difffoF2dayhr=GatherBy[#,8<IntegerPart[24FractionalPart[#[[1]]]]<17&]&/@difffoF2day]*)
+diffhmF2=diffs@@@Transpose@{dropbads[IhmF2],dropbads[hmF2]};
+difffoF2=diffs@@@Transpose@{dropbads[IfoF2],dropbads[foF2]};
+diffplots=ListPlot[#1,FrameLabel->{"Day of Year","hmF2 (km)",#2[[1]],None},PlotRange->{{62,68},{-300,300}},ImageSize->600]&@@@ 
+ Transpose@{diffhmF2~Join~difffoF2,stations};
+diffhmF2c=diffs@@@Transpose@{IhmF2,hmF2C};
+difffoF2c=diffs@@@Transpose@{IfoF2,foF2C};
+Dimensions[diffhmF2hr=GatherBy[#,IntegerPart[24FractionalPart[#[[1]]]]&]&/@tolocal[diffhmF2c]]
+Dimensions[difffoF2hr=GatherBy[#,IntegerPart[24FractionalPart[#[[1]]]]&]&/@tolocal[difffoF2c]]
+Dimensions[diffhmF2day=GatherBy[Join@@tolocal[diffhmF2c],IntegerPart[#[[1]]]&]]
+Dimensions[difffoF2day=GatherBy[Join@@tolocal[difffoF2c],IntegerPart[#[[1]]]&]]
+Dimensions[diffhmF2dayhr=GatherBy[#,8<IntegerPart[24FractionalPart[#[[1]]]]<17&]&/@diffhmF2day]
+Dimensions[difffoF2dayhr=GatherBy[#,8<IntegerPart[24FractionalPart[#[[1]]]]<17&]&/@difffoF2day]
 
 
 per[stationdata_,function_]:=#[[2]]/function[#[[1]]]&/@stationdata
@@ -70,11 +70,10 @@ applyhmF22[stat_]:={#[[1,1]],stat[Transpose[#][[2]]]}&/@#&/@diffhmF2dayhr
 applyfoF22[stat_]:={#[[1,1]],stat[Transpose[#][[2]]]}&/@#&/@difffoF2dayhr
 
 
-(* ::Input:: *)
-(*{meanshmF2,nshmF2,medianshmF2,stddevshmF2}=applyhmF2/@{Mean,Length,Median,StandardDeviation};*)
-(*{meansfoF2,nsfoF2,mediansfoF2,stddevsfoF2}=applyfoF2/@{Mean,Length,Median,StandardDeviation};*)
-(*{meanshmF22,nshmF22,medianshmF22,stddevshmF22}=applyhmF22/@{Mean,Length,Median,StandardDeviation};*)
-(*{meansfoF22,nsfoF22,mediansfoF22,stddevsfoF22}=applyfoF22/@{Mean,Length,Median,StandardDeviation};*)
+{meanshmF2,nshmF2,medianshmF2,stddevshmF2}=applyhmF2/@{Mean,Length,Median,StandardDeviation};
+{meansfoF2,nsfoF2,mediansfoF2,stddevsfoF2}=applyfoF2/@{Mean,Length,Median,StandardDeviation};
+{meanshmF22,nshmF22,medianshmF22,stddevshmF22}=applyhmF22/@{Mean,Length,Median,StandardDeviation};
+{meansfoF22,nsfoF22,mediansfoF22,stddevsfoF22}=applyfoF22/@{Mean,Length,Median,StandardDeviation};
 
 
 (*This function throws out outliers*)
@@ -110,43 +109,26 @@ correctionfoF2=f@@@Transpose@{dropbads@IfoF2,foF2I}
 (*
 /Tranpkbsposef@lliTranspose@{sample[#&]&/@Range[19],f,i}jkbj
 f]f]i, PlotStyle -> OrangejjjjjK1
+/Autodwi{1,8}jjjjjK1
 *)
 
-
-hs=Show[Plot[#1[x], {x, 62, 107}, PlotStyle -> Orange], 
-   Plot[#2[x], {x, 62, 107}, PlotStyle -> Red], 
-   Plot[#2[x + #3[[1]]] + #3[[2]], {x, 62, 107}], 
-   PlotRange -> {{62, 68}, Automatic}] & @@@ 
- Transpose@{hmF2I, dropbads@IhmF2, correctionhmF2}
-hf=Show[Plot[#1[x], {x, 62, 107}, PlotStyle -> Orange], 
-   Plot[#2[x], {x, 62, 107}, PlotStyle -> Red], 
-   Plot[#2[x + #3[[1]]] + #3[[2]], {x, 62, 107}], 
-   PlotRange -> {{62, 107}, Automatic}] & @@@ 
- Transpose@{hmF2I, dropbads@IhmF2, correctionhmF2}
-fs=Show[Plot[#1[x], {x, 62, 107}, PlotStyle -> Orange], 
-   Plot[#2[x], {x, 62, 107}, PlotStyle -> Red], 
-   Plot[#2[x + #3[[1]]] + #3[[2]], {x, 62, 107}], 
-   PlotRange -> {{62, 68}, Automatic}] & @@@ 
- Transpose@{foF2I, dropbads@IfoF2, correctionfoF2}
-ff=Show[Plot[#1[x], {x, 62, 107}, PlotStyle -> Orange], 
-   Plot[#2[x], {x, 62, 107}, PlotStyle -> Red], 
-   Plot[#2[x + #3[[1]]] + #3[[2]], {x, 62, 107}], 
-   PlotRange -> {{62, 107}, Automatic}] & @@@ 
- Transpose@{foF2I, dropbads@IfoF2, correctionfoF2}
+hp=Plot[{#1[x],#2[x],#2[x + #3[[1]]] + #3[[2]]},
+ {x, 62, 107}, PlotStyle -> {Orange,Red,Blue}] & @@@
+ Transpose@{hmF2I, dropbads@IhmF2, correctionhmF2};
+hs=Show[#,PlotRange->{{62,68},{100,400}}]&/@hp
+hf=Show[#,PlotRange->{{62,107},{100,400}}]&/@hp
+fp=Plot[{#1[x],#2[x],#2[x + #3[[1]]] + #3[[2]]},
+ {x, 62, 107}, PlotStyle -> {Orange,Red,Blue}] & @@@
+ Transpose@{foF2I, dropbads@IfoF2, correctionfoF2};
+fs=Show[#,PlotRange->{{62,68},{1,8}}]&/@fp
+ff=Show[#,PlotRange->{{62,107},{1,8}}]&/@fp
 
 
-(* ::Input:: *)
-(*(*Lots of stats on the differences*)*)
-(*Show/@hsmallshows*)
-(*Show/@hbigshows*)
-(*Show/@fsmallshows*)
-(*Show/@fbigshows*)
-(*Show@hsmallshows*)
-(*Show@hbigshows*)
-(*Show@fsmallshows*)
-(*Show@fbigshows*)
+Export["hs.pdf",Grid[Partition[hs,4]]];
+Export["fs.pdf",Grid[Partition[fs,4]]];
 
 
+(*Let's give us some fuctions to show everything for a station*)
 allhmF2[n_]:=Show[ListPlot[
   Transpose[dropbads[Transpose[{uttimebig, SamihmF2big}]][[n]]], 
   PlotStyle -> Hue@.2, Joined -> False], 
@@ -156,8 +138,6 @@ allhmF2[n_]:=Show[ListPlot[
  Plot[dropbads[IhmF2][[n]][x + correctionhmF2[[n]][[1]]] + 
    correctionhmF2[[n]][[2]], {x, 62, 107}, PlotStyle -> {Hue@.8,Thick}], 
  PlotRange -> {{62, 68}, {100, 400}}, ImageSize -> 800]
-
-
 allfoF2[n_]:=Show[ListPlot[
   Transpose[dropbads[Transpose[{uttimebig, SamifoF2big}]][[n]]], 
   PlotStyle -> Hue@.2, Joined -> False], 
@@ -169,10 +149,7 @@ allfoF2[n_]:=Show[ListPlot[
  PlotRange -> {{62, 68}, Automatic}, ImageSize -> 800]
 
 
-allfoF2[6]
-
-
-
-
+(*::Input::*)
+(*allfoF2[6]*)
 
 
